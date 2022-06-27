b8aa1bf8a0896ccef834d51cf7b454e3fcfe22cc
Nobleson <noblesityav@gmail.com>
2022-06-27 14:36:34 +0100

    add starter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 app.get("/", function(req, res){
-    res.send('Hello Reactor Demo')
+    res.send('Hello Reactor Demo Starter')
 })
 
 app.listen(5000, function(){
