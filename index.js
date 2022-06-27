b6ef31feb85e1e587a071b9c0a4470f591777d75
Nobleson <noblesityav@gmail.com>
2022-06-27 13:07:36 +0100

    add entry to server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+
+app.get("/", function(req, res){
+    res.send('Hello Reactor Demo')
+})
+
+app.listen(5000, function(){
+    console.log("Server is up and running")
+})
